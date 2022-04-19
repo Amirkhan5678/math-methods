dd38bdf83f92b8050dd45bae1e471bf3c57bddfc
Amirkhan5678 <sh4444qtr@gmail.com>
2022-04-19 06:11:04 +0500

    math method is done

---
+++ app.js
@@ -0,0 +1,100 @@
+// welcome to new assignment //
+
+// Question no 01 //
+
+//(A)//
+
+// var num = 3.4521;
+// alert(num);
+
+//(B)//
+
+// var num = 3.4521;
+// var roundNumber = Math.round(num);
+// alert(roundNumber);
+
+//(c)//
+
+// var num = 3.4521;
+// var floorNumber = Math.floor(num);
+// alert(roundNumber);
+
+//(D)//
+
+// var num = 3.4521;
+// var ceilNumber = Math.ceil(num);
+// alert(roundNumber);
+
+// Question no 02 //
+
+//(A)//
+
+// var number = -1.345;
+// alert(number);
+
+//(B)//
+
+// var num = -1.345;
+// var roundNumber = Math.round(num);
+// alert(roundNumber);
+
+//(C)//
+
+// var num = -1.345;
+// var floorNumber = Math.floor(num);
+// alert(roundNumber);
+
+
+//(D)//
+
+// var num = -1.345;
+// var ceilNumber = Math.ceil(num);
+// alert(roundNumber);
+
+// Question no 03 //
+
+// var abslNumber = Math.abs(-4);
+// alert(abslNumber);
+
+// Question no 04 //
+
+// var ranNumber = Math.random();
+// var imprNum = (ranNumber * 6) + 1;
+// var totalNum = Math.floor(imprNum);
+// alert(totalNum);
+
+
+// Question no 05 //
+
+// var ranNumber = Math.random();
+// var imprNum = (ranNumber * 2) + 1;
+// var totalNum = Math.floor(imprNum);
+// alert(totalNum);
+
+
+// Question no 06 //
+
+// var ranNumber = Math.random();
+// var imprNum = (ranNumber * 100) + 1;
+// var totalNum = Math.floor(imprNum);
+// alert(totalNum);
+
+// Question no 07 //
+
+// var weight = prompt("Enter your age");
+// document.write("your weight is " + weight);
+
+// Question no 08 //
+
+
+// var ranNumber = Math.random();
+// var imprNum = (ranNumber * 9) + 1;
+// var totalNum = Math.floor(imprNum);
+
+// for(var i =  prompt(""); i= totalNum.lenght; i++){
+//     if (i === totalNum){
+//         document.write("congratultaions");
+//     }
+// else { document.write("sorry plzz try again");
+// }
+// }
